b6100d09e40e3e5a0edcfa366b7aea8e4b8d960d
magthyhui <244626523@qq.com>
2017-03-31 23:09:45 +0800

    test

--- src/main/java/com/test/api/HelloController.java
+++ src/main/java/com/test/api/HelloController.java
@@ -1,14 +1,25 @@
 package com.test.api;
 
+import java.util.Map;
 import java.util.concurrent.Future;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.test.config.Constants;
+import com.test.entity.AuthRequest;
+
 @RestController
+@RequestMapping(value = Constants.URI_API_PREFIX)
 public class HelloController {
     
     
@@ -22,13 +33,11 @@ public class HelloController {
     
    */
     
-    @RequestMapping("/helloss")
-    public String indexs() throws Exception {
-        /*String sql ="select bt from fm_recgl where id = '20'";
-        String ls = jdbcTemplate1.queryForObject(sql,String.class);
-        return ls;*/
-        return null;
-    }
-
+	@RequestMapping(value = "/test", method = RequestMethod.POST)
+	public ResponseEntity<?> test(@RequestBody Map<String,Object> param) throws AuthenticationException{
+		
+		return ResponseEntity.ok("ok");
+		
+	}
     
 }
